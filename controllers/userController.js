07fa617c559e75e424fe2996b04631e3dd0056ac
Vijay-Psgtech <rvk.its@psgtech.ac.in>
2025-10-24 16:13:58 +0530

    User controller functions has been done.

---
+++ controllers/userController.js
@@ -0,0 +1,45 @@
+import User from "../models/userModel.js";
+
+// Get all users
+
+export const getAllUsers = async (req, res) => {
+  try {
+    const users = await User.find().sort({ createdAt: -1 });
+    res.status(200).json(users);
+  } catch (error) {
+    res.status(500).json({ message: "Server Error", error: error.message });
+  }
+};
+
+// Get user by ID
+
+export const getUserById = async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json({ message: "Server Error", error: error.message });
+  }
+};
+
+// Create new user
+
+export const createUser = async (req, res) => {
+  try {
+    const emailExists = await User.findOne({ email: req.body.email });
+    if (emailExists)
+      return res.status(400).json({ message: "Email already in use" });
+
+    const passwordHashed = await bcrypt.hash(req.body.password, 10);
+    req.body.password = passwordHashed;
+
+    const newUser = new User(req.body);
+    const savedUser = await newUser.save();
+    res.status(201).json(savedUser);
+  } catch (error) {
+    res.status(500).json({ message: "Server Error", error: error.message });
+  }
+};
