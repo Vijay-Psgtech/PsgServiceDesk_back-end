a9240dcb9b4efdde3588e4be405accd40e00c4b6
Vijay-Psgtech <rvk.its@psgtech.ac.in>
2025-10-24 16:30:49 +0530

    User routes setup has been done.

---
+++ routes/userRoute.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import { getAllUsers, createUser, getUserById } from '../controllers/userController';
+
+const router = express.Router();
+
+// Route to get all users
+router.get('/', getAllUsers);
+
+// Route to get a user by ID
+router.get('/:id', getUserById);
+
+// Route to create a new User
+router.post('/save', createUser);
+
+export default router;
--- server.js
+++ server.js
@@ -3,8 +3,12 @@ import dotenv from "dotenv";
 import cors from "cors";
 import connectDB from "./config/db.js";
 import cookieParser from "cookie-parser";
+import userRoutes from "./routes/userRoute.js";
 
 dotenv.config();
+
+// Connect to the database
+connectDB();
 
 const app = express();
 
@@ -12,9 +16,10 @@ app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 
-// Connect to the database
-connectDB();
+// Rooutes API
+app.use("/api/users", userRoutes);
 
+// Start the server
 app.listen(process.env.PORT || 5000, () => {
   console.log(`✅ Server is runnning on port ${process.env.PORT || 5000}`);
 });
